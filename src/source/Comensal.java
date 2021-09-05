66871f94f0c56b066e87469e9afe2747bc2405d3
Juan Sebastian Pinzon <js.pinzonr@uniandes.edu.co>
2021-09-05 16:58:34 -0500

    Carga de los datos de properties y algunas clases
    a

--- src/source/Comensal.java
+++ src/source/Comensal.java
@@ -2,4 +2,55 @@ package source;
 
 public class Comensal extends Thread{
 
+	private int contadorPlatos;
+	
+	private boolean cubiertoTipo1;
+	
+	private boolean cubiertoTipo2;
+	
+	
+
+	public Comensal()
+	{
+		contadorPlatos=0;
+		cubiertoTipo1=false;
+		cubiertoTipo2=false;
+
+	}
+	
+	
+	public void run() {
+		
+	}
+	
+	
+	
+	/** 
+	 * Puede ser de cualquier tipo, pero hay que tener en cuenta cuales ya tiene a disposición
+	 */
+	public synchronized void agarrarCubierto() {
+		
+		
+		
+	}
+	
+	
+	
+	/**
+	 * @throws InterruptedException 
+	 * 
+	 */
+	public synchronized void comerPlato() throws InterruptedException {
+		long tComiendo = (long) (Math.random()*2+3); //Tiempo aleatorio entre 3 y 5 segundos
+		sleep(tComiendo);
+		contadorPlatos++;
+		
+	}
+	
+	
+	public  synchronized void soltarCubiertos() {
+		
+	}
+	
+	
 }
--- src/source/Main.java
+++ src/source/Main.java
@@ -1,10 +1,57 @@
 package source;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public final static String pathDatos = "./data/data.properties";
+	
+	
+	
+	private static int numComensales;
+	
+	private static int numCubiertosT1;
+	
+	private static int numCubiertosT2;
+	
+	private static int numPlatos;
+	
+	private static int tamFregadero;
+	
+	
+	
+	public static void cargarDatos() throws Exception {
 
+		Properties datos = new Properties( );
+		FileInputStream in = new FileInputStream( pathDatos );
+		try{
+			datos.load( in );
+			in.close( );
+		}
+		catch(Exception e){
+			throw new Exception("Formato inválido.");
+		}
+		
+		numComensales = Integer.parseInt(datos.getProperty("concurrencia.numComensales"));
+		numCubiertosT1 = Integer.parseInt(datos.getProperty("concurrencia.numCubiertosT1"));
+		numCubiertosT2 = Integer.parseInt(datos.getProperty("concurrencia.numCubiertosT2"));
+		numPlatos = Integer.parseInt(datos.getProperty("concurrencia.numPlatos"));
+		tamFregadero = Integer.parseInt(datos.getProperty("concurrencia.tamFregadero"));
+		
+	}
+	
+	
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		cargarDatos();
+		System.out.println(tamFregadero);
+		
+		
+		
+		
+		
 	}
 
 }
