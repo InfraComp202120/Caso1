5607d35fd926cbe63be38953c38cfbde23d06a38
Juan Sebastian Pinzon <js.pinzonr@uniandes.edu.co>
2021-10-18 21:37:20 -0500

    Proyecto Funcional

--- Caso2/src/source/Main.java
+++ Caso2/src/source/Main.java
@@ -5,19 +5,20 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main extends Thread {
 
 	public boolean tipoT;
 
-	//public final static String pathDatos = "./data/referencias8_16_75.txt";
-	//public final static String pathDatos = "./data/referencias8_32_75.txt";
-	//public final static String pathDatos = "./data/referencias8_64_75.txt";
-    public final static String pathDatos = "./data/referencias8_128_75.txt";
+	public final static String pathDatos0 = "./data/referencias2_16_75.txt";
+	public final static String pathDatos1 = "./data/referencias2_32_75.txt";
+	public final static String pathDatos2 = "./data/referencias2_64_75.txt";
+	public final static String pathDatos3 = "./data/referencias2_128_75.txt";
 
 	//public final static String pathDatos = "./data/prueba0.txt";
-	
-	
+
+
 	private static int numMarcos;
 
 
@@ -60,7 +61,7 @@ public class Main extends Thread {
 			int pag = secPag[i];
 			String tipo = secTipo[i];
 			//System.out.println("La ref es: "+ pag+"  el tipo es  "+tipo);
-			
+
 			// TIPO R
 			if(tipo.equalsIgnoreCase("r"))
 			{
@@ -77,42 +78,42 @@ public class Main extends Thread {
 				sleep(1); //Duerme 1 ms
 			} catch (InterruptedException e) {}
 		}
-		
-		
+
+
 		enProceso=false;
 
 		System.out.println("El numero de fallos fue: "+ fallos);
 	}
 
 	public void procesoLectura(int pag) {
-		
+
 		//  La página NO ESTA EN MARCO
 		if(!estaEnMarco(pag)) {
-			
+
 			// El Marco está vacio
 			if(meterEnVacio(pag)) {	
 				fallos++;
 				cambiarR(pag);
+				//System.out.println("Se agrego un fallo por vacio lectura con la pg: "+pag);
 			}
 
 			// No hay vacio en el marco
 			else {
 				cambioEnMarco(pag);
 			}
-			System.out.println("Se agrego un fallo por lectura con la pg: "+pag);
 		}
-		
+
 		//  La página sí está en el marco
 		else {
 			enMarco(pag);
 		}
 	}
-	
+
 	public synchronized void procesoEscritura(int pag) {
 		if(estadoPag[pag][2]==0) {
 			estadoPag[pag][2]=1;
-			fallos++;
-			System.out.println("Se agrego un fallo por escritura con la pg: "+pag);
+			//fallos++;
+			//System.out.println("Se agrego un fallo por escritura con la pg: "+pag);
 		}
 	}
 	public synchronized void enMarco(int pag) {
@@ -123,7 +124,7 @@ public class Main extends Thread {
 			cambiarR(pag);
 		}
 	}
-	
+
 	public boolean estaEnMarco(int pag) {
 		for (int m : marcos) {
 			if(pag==m)
@@ -131,7 +132,7 @@ public class Main extends Thread {
 		}
 		return false;
 	}
-	
+
 	public boolean meterEnVacio(int pag) {
 		for (int i = 0; i < numMarcos; i++) {
 			if(marcos[i]==-1) {
@@ -141,16 +142,16 @@ public class Main extends Thread {
 		}
 		return false;
 	}
-	
-	
+
+
 	public synchronized void cambioEnMarco(int pag) {
 		int indiceMenor=-1;
 		int grupoMenor=-1;
-		
+
 		for (int i = 0; i < numMarcos; i++) {
 			int m = marcos[i];
 			int grupo = estadoPag[m][1]*2 + estadoPag[m][2];
-			
+
 			if (grupo==0) {
 				indiceMenor=i;
 				break;
@@ -161,18 +162,19 @@ public class Main extends Thread {
 				grupoMenor=grupo;
 			}
 		}
-		
+		int pagEliminada = marcos[indiceMenor];
+		estadoPag[pagEliminada][2]=0;
+
 		marcos[indiceMenor] = pag;
-		
 		if(estadoPag[pag][1]==0)
 		{
 			cambiarR(pag);
+		}	
 		fallos++;
-		
-		}
+		//System.out.println("Se agrego un fallo por cambioEnMarco lectura con la pg: "+pag+ "  se eliminó la pg "+pagEliminada);
 	}
-	
-	
+
+
 	public synchronized void cambiarR(int pag) {
 		estadoPag[pag][1]=1;
 	}
@@ -181,10 +183,10 @@ public class Main extends Thread {
 	public synchronized void cambiarM(int pag) {
 		estadoPag[pag][2]=1;
 	}
-*/
+	 */
 
-	
-	
+
+
 	/*
 	 * Metodo de run para el thread 2 
 	 */
@@ -195,7 +197,6 @@ public class Main extends Thread {
 			try {
 				sleep(20); //Duerme 20 ms
 			} catch (InterruptedException e) {}	
-			//System.out.println("LIMPIANDO JAJA");
 			limpiarR();
 		}
 
@@ -204,15 +205,23 @@ public class Main extends Thread {
 
 	public synchronized void limpiarR() {
 		for (int i = 0; i <numMarcos; i++) {
-
 			estadoPag[i][1]=0;
 		}
+		//System.out.println("---------------------LIMPIEZA-----------------");
 	}
 
 
 	public static void cargarDatos() throws Exception {
 
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("------- Bienvenido al simulador de Memoria Virtual -------");
+		System.out.println("A continuación elija el archivo que desea cargar para la configuración del sistema: "
+				+ "\n	0: referencias8_16"
+				+ "\n	1: referencias8_32"
+				+ "\n	2: referencias8_64"
+				+ "\n	3: referencias8_128");
+		int p = sc.nextInt();
+		String pathDatos = ((p==0)?pathDatos0:(p==1)?pathDatos1:(p==2)?pathDatos2:pathDatos3);
 		File file = new File(pathDatos);
 		BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -236,31 +245,31 @@ public class Main extends Thread {
 
 
 	public static void main(String[] args) {
+		while(true) {
+			enProceso=true;
+			try {
+				cargarDatos();
+			} catch (Exception e) { }
 
-		enProceso=true;
-		try {
-			cargarDatos();
-		} catch (Exception e) { }
+			estadoPag = new int[numPag][3];
+			for (int i = 0; i < numPag; i++) {
+				estadoPag[i][1]=0;
+				estadoPag[i][2]=0;
+			}
+			marcos = new int[numMarcos];
+			for (int i = 0; i < marcos.length; i++) {
+				marcos[i]=-1;
+			}
 
-		estadoPag = new int[numPag][3];
-		for (int i = 0; i < numPag; i++) {
-			estadoPag[i][1]=0;
-			estadoPag[i][2]=0;
+			fallos=0;
+
+			Main th1 = new Main(false);
+			Main th2 = new Main(true);
+
+			th1.start();
+			th2.start();
+
 		}
-		marcos = new int[numMarcos];
-		System.out.println(numMarcos);
-		for (int i = 0; i < marcos.length; i++) {
-			marcos[i]=-1;
-		}
-		
-		fallos=0;
-
-		Main th1 = new Main(false);
-		Main th2 = new Main(true);
-
-		th1.start();
-		th2.start();
-
 	}
 
 }
