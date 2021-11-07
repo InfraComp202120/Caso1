f0ed028abbf2d07266cc412874c6d4416e300036
Alejandro Alcaraz <a.alcaraz@uniandes.edu.co>
2021-11-07 16:01:22 -0500

    Arreglo symmetric

--- Caso3/src/Source/Symmetric.java
+++ Caso3/src/Source/Symmetric.java
@@ -1,164 +1,68 @@
 package Source;
 
-// Java program to implement the
-// encryption and decryption
-
-import java.security.SecureRandom;
-import java.util.Scanner;
-
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+ 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec
-	.IvParameterSpec;
-import javax.xml.bind
-	.DatatypeConverter;
-
-// Creating the symmetric
-// class which implements
-// the symmetric
+import javax.crypto.spec.SecretKeySpec;
+ 
 public class Symmetric {
-
-	private static final String AES
-		= "AES";
-
-	// We are using a Block cipher(CBC mode)
-	private static final String AES_CIPHER_ALGORITHM
-		= "AES/CBC/PKCS5PADDING";
-
-	private static Scanner message;
-
-	// Function to create a
-	// secret key
-	public static SecretKey createAESKey()
-		throws Exception
-	{
-		SecureRandom securerandom
-			= new SecureRandom();
-		KeyGenerator keygenerator
-			= KeyGenerator.getInstance(AES);
-
-		keygenerator.init(128, securerandom);
-		SecretKey key
-			= keygenerator.generateKey();
-
-		return key;
-	}
-
-	// Function to initialize a vector
-	// with an arbitrary value
-	public static byte[] createInitializationVector()
-	{
-
-		// Used with encryption
-		byte[] initializationVector
-			= new byte[16];
-		SecureRandom secureRandom
-			= new SecureRandom();
-		secureRandom.nextBytes(initializationVector);
-		return initializationVector;
-	}
-
-	// This function takes plaintext,
-	// the key with an initialization
-	// vector to convert plainText
-	// into CipherText.
-	public static byte[] do_AESEncryption(
-		String plainText,
-		SecretKey secretKey,
-		byte[] initializationVector)
-		throws Exception
-	{
-		Cipher cipher
-			= Cipher.getInstance(
-				AES_CIPHER_ALGORITHM);
-
-		IvParameterSpec ivParameterSpec
-			= new IvParameterSpec(
-				initializationVector);
-
-		cipher.init(Cipher.ENCRYPT_MODE,
-					secretKey,
-					ivParameterSpec);
-
-		return cipher.doFinal(
-			plainText.getBytes());
-	}
-
-	// This function performs the
-	// reverse operation of the
-	// do_AESEncryption function.
-	// It converts ciphertext to
-	// the plaintext using the key.
-	public static String do_AESDecryption(
-		byte[] cipherText,
-		SecretKey secretKey,
-		byte[] initializationVector)
-		throws Exception
-	{
-		Cipher cipher
-			= Cipher.getInstance(
-				AES_CIPHER_ALGORITHM);
-
-		IvParameterSpec ivParameterSpec
-			= new IvParameterSpec(
-				initializationVector);
-
-		cipher.init(
-			Cipher.DECRYPT_MODE,
-			secretKey,
-			ivParameterSpec);
-
-		byte[] result
-			= cipher.doFinal(cipherText);
-
-		return new String(result);
-	}
-
-	// Driver code
-	public static void main(String args[])
-		throws Exception
-	{
-		SecretKey Symmetrickey
-			= createAESKey();
-
-		System.out.println(
-			"The Symmetric Key is :"
-			+ DatatypeConverter.printHexBinary(
-				Symmetrickey.getEncoded()));
-
-		byte[] initializationVector
-			= createInitializationVector();
-
-		String plainText
-			= "A elle le gusta la gasolina";
-
-		// Encrypting the message
-		// using the symmetric key
-		byte[] cipherText
-			= do_AESEncryption(
-				plainText,
-				Symmetrickey,
-				initializationVector);
-
-		System.out.println(
-			"The ciphertext or "
-			+ "Encrypted Message is: "
-			+ DatatypeConverter.printHexBinary(
-				cipherText));
-
-		// Decrypting the encrypted
-		// message
-		String decryptedText
-			= do_AESDecryption(
-				cipherText,
-				Symmetrickey,
-				initializationVector);
-
-		System.out.println(
-			"Your original message is: "
-			+ decryptedText);
-	}
+ 
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+ 
+    public static void setKey(String myKey) 
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16); 
+            secretKey = new SecretKeySpec(key, "AES");
+        } 
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } 
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public static String encrypt(String strToEncrypt, String secret) 
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+ 
+    public static String decrypt(String strToDecrypt, String secret) 
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
 }
 
 
